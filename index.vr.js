44516499a607e47d41e47a416134cf819f1b91d7
ErkanSensei <erkansen14@hotmail.com>
2017-03-20 16:27:24 -0500

    initial commit

---
+++ index.vr.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+class App extends React.Component {
+  render() {
+    return (
+    <View>
+      <Pano source={asset('chess-world.jpg')} />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        width: 2,
+        alignItems: 'stretch',
+        transform: [{translate: [-1, 1, -5]}],
+      }}>
+
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
+      </View>
+
+    </View>
+    </View>
+    )
+  }
+};
+
+AppRegistry.registerComponent('reactvr-harrypotter', () => App);
